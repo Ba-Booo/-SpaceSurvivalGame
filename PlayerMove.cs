fd9497ba037b7c29696e013fadf2c249489c63b5
Ba-Booo <123147457+Ba-Booo@users.noreply.github.com>
2023-07-13 23:00:04 +0900

    Update PlayerMove.cs
    add jump

--- PlayerMove.cs
+++ PlayerMove.cs
@@ -10,9 +10,14 @@ public class PlayerMove : MonoBehaviour
     float MoveX;
     float MoveZ;
 
+    public float Speed;
+    public float Jump;
+
 
     void Start()
     {
+
+        rb = GetComponent<Rigidbody>();
         
     }
 
@@ -20,8 +25,14 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
 
-        MoveX = Input.GetAxis("Horizontal") * Time.deltaTime;   //좌우 움직임
-        MoveZ = Input.GetAxis("Vertical") * Time.deltaTime;   //좌우 움직임
+        MoveX = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;   //좌우 움직임
+        MoveZ = Input.GetAxis("Vertical") * Speed * Time.deltaTime;   //앞뒤 움직임
+        
+
+        if ( Input.GetKeyDown( KeyCode.Space ) )        //점프
+        {
+            rb.AddForce(transform.up * Jump, ForceMode.Impulse);
+        }
 
         transform.position = new Vector3(transform.position.x + MoveX, transform.position.y, transform.position.z + MoveZ );
 
