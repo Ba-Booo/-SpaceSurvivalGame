784658b5c84340e4c3509d2e82882d3b34515b15
Ba-Booo <123147457+Ba-Booo@users.noreply.github.com>
2023-08-03 22:34:01 +0900

    Update RandomOre.cs

--- RandomOre.cs
+++ RandomOre.cs
@@ -11,12 +11,19 @@ public class RandomOre : MonoBehaviour
 
     public float areaSize;  //범위
 
+    
+    float playerX; 
+    float playerZ;
+    
     void Start()
     {
+        
+        playerX = transform.position.x;
+        playerZ = transform.position.z;
 
         for (int i = 0; i < count; i++)
         {
-            Instantiate(GameObject, new Vector3(Random.Range(areaSize * -1, areaSize), 0, Random.Range(areaSize * -1, areaSize)), Quaternion.identity );    //복제
+            Instantiate(GameObject, new Vector3(Random.Range(playerX - areaSize, playerX + areaSize), 10, Random.Range(playerZ - areaSize, playerZ + areaSize)), Quaternion.identity );    //복제
         }
 
     }
