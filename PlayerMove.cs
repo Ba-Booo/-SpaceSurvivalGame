4d200f6ba97d5fabfc71ebe15e9276e72778ac8e
Ba-Booo <123147457+Ba-Booo@users.noreply.github.com>
2023-11-27 13:39:06 +0900

    Update PlayerMove.cs
    fix bug
    delete oxygen gauge variable

--- PlayerMove.cs
+++ PlayerMove.cs
@@ -5,25 +5,20 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
 
+    Vector3 moveDir;
     Rigidbody rb;       //중력
     public Transform cameraDirection;  //카메라 방향
 
-    float MoveX, MoveZ;
-
+    float MoveX, MoveZ;     //움직임
     public float Speed;
+
+    public bool JumpPrevention;    //점프
     public float JumpPower;
-    public float NowOxygen;
-    public float MaxOxygen;
-
-
 
     void Start()
-    {
-        
-        rb = GetComponent<Rigidbody>();
-        
+    {    
+        rb = GetComponent<Rigidbody>();        
     }
-
 
     void Update()
     {   
@@ -31,30 +26,36 @@ public class PlayerMove : MonoBehaviour
         Jump();
     }
 
-
-
     void Move()
     {
 
-        MoveX = Input.GetAxis("Horizontal");   //좌우 움직임
-        MoveZ = Input.GetAxis("Vertical");   //앞뒤 움직임
+        MoveX = Input.GetAxis("Horizontal");    //좌우 움직임
+        MoveZ = Input.GetAxis("Vertical");      //앞뒤 움직임
 
-        Vector3 moveDir = (Vector3.forward * MoveZ) + (Vector3.right * MoveX);
-
+        moveDir = (Vector3.forward * MoveZ) + (Vector3.right * MoveX);
         transform.Translate( moveDir * Speed * Time.deltaTime, Space.Self );
 
-        transform.rotation = Quaternion.Euler( 0, cameraDirection.eulerAngles.y, 0 );
+        transform.rotation = Quaternion.Euler( 0, cameraDirection.eulerAngles.y, 0 );   //회전
 
     }
 
     void Jump()
     {
 
-       if ( Input.GetKeyDown( KeyCode.Space ) )        //점프
+        if( Input.GetKeyDown( KeyCode.Space ) && JumpPrevention == true )        //점프
         {
             rb.AddForce(transform.up * JumpPower, ForceMode.Impulse);
+            JumpPrevention = false;
         } 
 
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            JumpPrevention = true;
+        }
     }
      
 }
