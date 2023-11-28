66e7c6ad95bb2a225fcb5554287c8edcba5a44af
Ba-Booo <123147457+Ba-Booo@users.noreply.github.com>
2023-11-28 10:42:25 +0900

    Update CamMove.cs
    delete useless thing
    add programs related to UI

--- CamMove.cs
+++ CamMove.cs
@@ -6,42 +6,34 @@ public class CamMove : MonoBehaviour
 {
 
     public Transform cameraPosition;
+    public UIScript blind;
 
     public float sensX;
     public float sensY; //X와 Y의 대한 감도
 
-    public Transform orientation;
-
     float xRotation;
     float yRotation;
 
-    private void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+    void Update()
+    {   
 
+        if(blind.blink != true)
+        {
+            transform.position = new Vector3( cameraPosition.position.x, cameraPosition.position.y + 1.4f, cameraPosition.position.z );
 
-    private void Update()
-    {    
+            //마우스값 input
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-        transform.position = new Vector3( cameraPosition.position.x, cameraPosition.position.y + 1.4f, cameraPosition.position.z );
+            yRotation += mouseX;
 
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        //마우스값 input
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
-
-        yRotation += mouseX;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        //화면 회전
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0,yRotation, 0);
+            //화면 회전
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        }
 
     }
-
-    
+  
 }
